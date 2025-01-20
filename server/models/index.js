dad4ea41e98e4540510283d8f9b91ab6c64f4def
Abhi-P22 <132135625+Abhi-P22@users.noreply.github.com>
2025-01-21 05:00:55 +0530

    Update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -6,7 +6,12 @@ const config = require("../config/config.json").production;
 const sequelize = new Sequelize(
   process.env.DB_NAME, 
   process.env.DB_USERNAME,
-  process.env.DB_PASSWORD
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT || 3306,
+  }
 );
 
 const db = {};
