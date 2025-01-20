7a2103ecc0997db80142d579555483d49345f2a1
Abhi-P22 <132135625+Abhi-P22@users.noreply.github.com>
2025-01-21 03:25:07 +0530

    Update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
-const config = require("../config/config.json").development;
+const config = require("../config/config.json").production;
 
 const sequelize = new Sequelize(
   config.database,
